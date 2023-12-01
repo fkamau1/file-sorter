7bf8d98b5892704d42088a4cef0fe4e0f0ec1523
fkamau <fredricoinc@gmail.com>
2023-11-30 21:18:27 -0600

    optimize file moves

--- file-sorter.py
+++ file-sorter.py
@@ -1,42 +1,44 @@
 import os
 from time import sleep
 from shutil import move
-from pathlib import Path, rglob 
-from watchdog.observers import Observer 
+from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-dest_folder = "/Users/Fredrick/Downloads"
-src_folder = "/Users/Fredrick/Downloads/Documents"
+src_folder = "/Users/Fredrick/Downloads"
+dest_folder = "/Users/Fredrick/Downloads/Documents"
 
 class DocumentMoverHandler(FileSystemEventHandler):
-
-    def on_created(self, event):
-        self.on_modified(event)
-
-    def on_modified(self, event):
-        for entry in os.scandir("/Users/Fredrick/Downloads/Documents"):
-            if (entry.is_file() and 
-                entry.name.endswith((".doc", ".docx", ".pdf"))):
+    def move_existing_files(self):
+        for entry in os.scandir(src_folder):
+            if entry.is_file() and entry.name.endswith((".doc", ".docx", ".pdf")):
                 move(entry.path, dest_folder)
                 print(f"Moved {entry.name} to {dest_folder}")
 
-    def move_existing(self, src_folder):
-    
-        for src_path in src_folder.rglob('*'):
-            if src_path.is_file():
-                dest_path = dest_folder + '/' + src_path.name
-                if os.path.exists(dest_path):
-                    os.remove(dest_path) 
-                move(str(src_path), dest_path)
+    def on_created(self, event):
+        self.move_all_files()
+
+    def on_modified(self, event):
+        self.move_all_files()
+
+    def move_all_files(self):
+        for entry in os.scandir(src_folder):
+            if entry.is_file() and entry.name.endswith((".doc", ".docx", ".pdf")):
+                move(entry.path, dest_folder)
+                print(f"Moved {entry.name} to {dest_folder}")
 
 if __name__ == "__main__":
     event_handler = DocumentMoverHandler()
+
+    # Move existing files immediately upon starting the script
+    event_handler.move_existing_files()
+
     observer = Observer()
-    observer.schedule(event_handler, "/Users/Fredrick/Downloads/Documents", recursive=True)
+    observer.schedule(event_handler, src_folder, recursive=True)
     observer.start()
+
     try:
         while True:
-            sleep(10)
+            sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
