fd86d8aa28ff368e7785b9125e7fe36cd4ab13de
fkamau <fredricoinc@gmail.com>
2024-01-06 16:35:05 -0600

    Script improvments

--- file-sorter.py
+++ file-sorter.py
@@ -4,14 +4,20 @@ from shutil import move
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-src_folder = "/Users/Fredrick/Downloads/Documents"
-dest_folder = "/Users/Fredrick/Downloads"
+src_folder = "C:\\Users\\Fredrick\\Downloads"
+dest_folder = "C:\\Users\\Fredrick\\Downloads\\Documents"
+
+#Destination folder validation
+if not os.path.exists(dest_folder):
+    os.makedirs(dest_folder)
+    print(f"Destination folder {dest_folder} created.")
 
 class DocumentMoverHandler(FileSystemEventHandler):
     def move_existing_files(self):
         for entry in os.scandir(src_folder):
             if entry.is_file() and entry.name.endswith((".doc", ".docx", ".pdf")):
-                move(entry.path, dest_folder)
+                #move(entry.path, dest_folder)
+                self.move_file(entry)
                 print(f"Moved {entry.name} to {dest_folder}")
 
     def on_created(self, event):
@@ -23,8 +29,18 @@ class DocumentMoverHandler(FileSystemEventHandler):
     def move_all_files(self):
         for entry in os.scandir(src_folder):
             if entry.is_file() and entry.name.endswith((".doc", ".docx", ".pdf")):
-                move(entry.path, dest_folder)
+                #move(entry.path, dest_folder)
+                self.move_file(entry)
                 print(f"Moved {entry.name} to {dest_folder}")
+
+    def move_file(self, entry):
+        # Check if the source and destination folders are different
+        if entry.path != dest_folder:
+            try:
+                move(entry.path, os.path.join(dest_folder, entry.name))
+                print(f"Moved {entry.name} to {dest_folder}")
+            except Exception as e:
+                print(f"Error moving {entry.name}: {e}")
 
 if __name__ == "__main__":
     event_handler = DocumentMoverHandler()
